ecda2a59b4c3515718d85683f2124186cc3cd365
Harshita saini <harshitasaini631@gmail.com>
2026-08-27 11:34:44 +0530

    Implemented file operation Create file by using writeFile() method

--- fileManager.js
+++ fileManager.js
@@ -3,13 +3,25 @@ const fs = require("fs");
 
 // read operation
 
-fs.readFile("./text.txt","utf-8",(err,data)=>{
-    if(err){
-        console.log("Something went wrong");
-    } else{
-        console.log(data);
+// fs.readFile("./text.txt","utf-8",(err,data)=>{
+//     if(err){
+//         console.log("Something went wrong");
+//     } else{
+//         console.log(data);
         
+//     }
+// });
+
+//write operation
+fs.writeFile("./text.txt","New written file",(err)=>{
+    if(err){
+        console.log("something went wrong");
+    } else{
+        console.log("File written successfully");
     }
 });
+
+
+
 
 
