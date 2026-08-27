be84becb1f739aa5a8f671c7374f517d121315dc
Harshita saini <harshitasaini631@gmail.com>
2026-08-27 13:05:47 +0530

    Storing dice history  in dice.txt

--- dice.js
+++ dice.js
@@ -6,4 +6,14 @@ let output = "";
 for (let i = 0; i < 10; i++) {
     const ans1 = crypto.randomInt(1, 7);
     console.log("Dice:", ans1);
+     output += ("\nDice:"+ ans1);
 }
+
+
+fs.writeFile("./dice.txt", output, (err) => {
+    if (err) {
+        console.log("Something went wrong:", err);
+    } else {
+        console.log("Dice results saved successfully!");
+    }
+});
